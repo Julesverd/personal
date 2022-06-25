4dddc50489ae7c1939009e6a97a2ca3f23a19412
Jules Verdadero <101442766+Julesverd@users.noreply.github.com>
2022-06-25 01:22:50 -0400

    App.js removed component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, from 'react';
 import { HashRouter as Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
